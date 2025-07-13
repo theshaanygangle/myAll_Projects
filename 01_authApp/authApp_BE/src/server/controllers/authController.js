1448fba8ab03af72fbe61d1a4d233d2fa842d060
Shaany Gangle <shaanygangle21@gmail.com>
2025-07-13 14:48:37 +0530

    Added authController

--- 01_authApp/authApp_BE/src/server/controllers/authController.js
+++ 01_authApp/authApp_BE/src/server/controllers/authController.js
@@ -1,6 +1,8 @@
 import { request } from "express"
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
+import userModel from '../models/userModel.js'
+import mongoose from "mongoose"
 
 export const register = async (req,res)=> {
       const {name , email , password } = req.body;
@@ -11,12 +13,104 @@ export const register = async (req,res)=> {
                   message : 'Missing Credentials'
             })
       }
-      try {
+      try { 
+            const existingUser = await userModel.findOne(email); 
+            if (existingUser) {
+                  return res.json({
+                        success : false,
+                        message : 'User  already exists!'
+                  })
+            }
+            //Creating New User
             const hashedPassword = await bcrypt.hash(password,10) // 10 signifies the encription level of password provided by the user
+            const user = new userModel({name , email , password : hashedPassword})
+            await user.save();
+
+            //Generating Token
+            const token = jwt.sign({id : user._id },process.env.JWT_SECRET, { expiresIn : '7d'})
+
+            res.cookie('token' , token, {
+                  httpOnly : true,
+                  secure : process.env.NODE_ENV === 'production',
+                  sameSite : process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                  maxAge : 7 * 24 * 60 * 60 * 1000,
+            })
+
       } catch (error) {
             return res.json({
                   success : false,
                   message : error.message, //error.message will return error message
+            })
+      }
+}
+
+export const login = async (req,res) => {
+      const {email , password} = req.body;
+
+      if (!email || !password) {
+            return res.json({
+                  success : false,
+                  message : "Email and Password Required!"
+            })
+      }
+
+      try {
+            const user = await userModel.findOne({email});
+
+            if (user) {
+                  return res.json({
+                        success : false,
+                        message :  "invalid Email!"
+                  })
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                  return res.json({
+                        success : false,
+                        message :  "invalid Password!"
+                  })
+            }
+
+            const token = jwt.sign({id : user._id },process.env.JWT_SECRET, { expiresIn : '7d'})
+
+            res.cookie('token' , token, {
+                  httpOnly : true,
+                  secure : process.env.NODE_ENV === 'production',
+                  sameSite : process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                  maxAge : 7 * 24 * 60 * 60 * 1000,
+            });
+
+            return res.json({
+                  success : true,
+            })
+
+
+      } catch (error) {
+            return res.json({
+                  success : false,
+                  message : error.message
+            })
+      }
+}
+
+export const logout = async (req, res) => {
+      try {
+            res.clearCookie('token', {
+                   httpOnly : true,
+                  secure : process.env.NODE_ENV === 'production',
+                  sameSite : 'strict',
+            })
+            return res.json({
+                  success : true,
+                  message : "Logged Out!"
+            })
+
+      } catch (error) {
+            return res.json({
+                  success : false,
+                  message : error.message
             })
       }
 }
